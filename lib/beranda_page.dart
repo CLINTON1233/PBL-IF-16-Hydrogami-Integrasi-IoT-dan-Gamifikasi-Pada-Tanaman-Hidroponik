d061097904c7bf1e58ae9899b0ee98e8c3b63b78
CLINTON1233 <clintonalfaro664@gmail.com>
2024-11-07 14:54:20 +0700

    Halaman Beranda

--- lib/beranda_page.dart
+++ lib/beranda_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hydrogami2/login_page.dart';
+import 'package:hydrogami2/notifikasi_page.dart';
 
 class BerandaPage extends StatefulWidget {
   const BerandaPage({super.key});
@@ -8,8 +11,253 @@ class BerandaPage extends StatefulWidget {
 }
 
 class _BerandaPageState extends State<BerandaPage> {
+  int _bottomNavCurrentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF24D17E),
+        elevation: 0,
+        toolbarHeight: 0,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 1),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Row untuk logo dan teks "Hi, Anton!"
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/hydrogami_logo.png',
+                          width: 85,
+                          height: 85,
+                        ),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Hi, Anton!',
+                          style: GoogleFonts.roboto(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.notification_add,
+                              color: Colors.black),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const NotifikasiPage()),
+                            );
+                          },
+                        ),
+                        const SizedBox(width: 20),
+                        IconButton(
+                          icon: const Icon(Icons.logout, color: Colors.black),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginPage()),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(width: 30),
+                        const Icon(
+                          Icons.location_on,
+                          size: 30,
+                          color: Colors.green,
+                        ),
+                        const SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Batam',
+                              style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'Cerah, Berawan',
+                              style: GoogleFonts.roboto(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    // Kolom untuk cuaca, di posisi kanan sejajar dengan icon logout
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.wb_sunny,
+                          size: 18,
+                          color: Colors
+                              .orange, // Opsional: Ubah warna untuk merepresentasikan cuaca
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          '35°C',
+                          style: GoogleFonts.roboto(
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(21.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 15,
+                // Mengatur rasio aspek untuk memperpanjang tinggi kartu
+                childAspectRatio: 3 / 4,
+                children: <Widget>[
+                  _buildCard('Monitoring\nReal-Time'),
+                  _buildCard('Gamifikasi'),
+                  _buildCard('Panduan'),
+                  _buildCard('Kelola Profile'),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: _buildBottomNavigation(),
+    );
+  }
+
+  // Fungsi untuk membuat Card dengan parameter teks
+  Widget _buildCard(String title) {
+    return Card(
+      color: const Color(0xFF29CC74),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4, // Opsional: Menambahkan bayangan
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () {
+          // Tambahkan fungsionalitas onTap di sini
+        },
+        child: Center(
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16, // Opsional: Menambahkan ukuran font
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Fungsi untuk membuat Bottom Navigation Bar dengan sudut melengkung
+  Widget _buildBottomNavigation() {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF24D17E),
+        onTap: (index) {
+          setState(() {
+            _bottomNavCurrentIndex = index;
+          });
+        },
+        currentIndex: _bottomNavCurrentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.notification_add,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.notification_add,
+              color: Colors.white,
+            ),
+            label: 'Notifikasi',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.assignment,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.assignment,
+              color: Colors.white,
+            ),
+            label: 'Panduan',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            label: 'Akun',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+      ),
+    );
   }
 }
---
+++ lib/panduan_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PanduanPage extends StatefulWidget {
+  const PanduanPage({super.key});
+
+  @override
+  State<PanduanPage> createState() => _PanduanPageState();
+}
+
+class _PanduanPageState extends State<PanduanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

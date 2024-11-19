9d80c46dc051f9b49f9c11e3440a83f6efa58d8d
Skyrims04 <ghazyabid12@gmail.com>
2024-11-19 17:49:54 +0700

    reward

---
+++ lib/reward_page.dart
@@ -0,0 +1,130 @@
+import 'package:application_hydrogami/beranda_page.dart';
+import 'package:application_hydrogami/notifikasi_page.dart';
+import 'package:application_hydrogami/panduan_page.dart';
+import 'package:application_hydrogami/profil_page.dart';
+import 'package:flutter/material.dart';
+
+class RewardPage extends StatefulWidget {
+  const RewardPage({super.key});
+
+  @override
+  State<RewardPage> createState() => _RewardPageState();
+}
+
+class _RewardPageState extends State<RewardPage> {
+  // Menambahkan variabel untuk menyimpan indeks BottomNavigation
+  int _bottomNavCurrentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF24D17E),
+        elevation: 0,
+        toolbarHeight: 0,
+      ),
+      body: const Center(
+        child: Text('Halaman Reward'),
+      ),
+      bottomNavigationBar:
+          _buildBottomNavigation(), // Menggunakan bottom navigation bar
+    );
+  }
+
+  // Fungsi untuk membuat BottomNavigationBar
+  Widget _buildBottomNavigation() {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(10),
+        topRight: Radius.circular(10),
+      ),
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF24D17E),
+        onTap: (index) {
+          setState(() {
+            _bottomNavCurrentIndex = index;
+          });
+
+          // Menangani navigasi berdasarkan indeks
+          switch (index) {
+            case 0: // Beranda
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const BerandaPage()),
+              );
+              break;
+            case 1: // Notifikasi
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const NotifikasiPage()),
+              );
+              break;
+            case 2: // Panduan
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const PanduanPage()),
+              );
+              break;
+            case 3: // Profil
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const ProfilPage()),
+              );
+              break;
+          }
+        },
+        currentIndex: _bottomNavCurrentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.notification_add,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.notification_add,
+              color: Colors.white,
+            ),
+            label: 'Notifikasi',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.assignment,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.assignment,
+              color: Colors.white,
+            ),
+            label: 'Panduan',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            label: 'Akun',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+      ),
+    );
+  }
+}
